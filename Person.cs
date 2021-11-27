6c87fcdc5798d425ad6fdb7d6775479e583b4cd9
robvanpamel <rob.vanpamel@gmail.com>
2021-11-27 07:10:15 +0000

    initial version

---
+++ Person.cs
@@ -0,0 +1,10 @@
+﻿namespace blog_nullable_reference_types;
+public class Person
+{
+    public FullName Name { get; set; }
+}
+
+public class FullName
+{
+    public string FirstName { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using blog_nullable_reference_types;
+
+var person = new Person();
+Console.WriteLine($"Name:{person.Name.FirstName}");
